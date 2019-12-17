e0df23561753e67dc5fc3cb5d67148bad2eab2a5
Frank Zheng <fzheng98@gmail.com>
2019-12-16 20:30:29 -0500

    removed db.drop_all() in init

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -79,7 +79,6 @@ def create_app():
     app.register_blueprint(questions)
 
     with app.app_context():
-        db.drop_all()
         db.create_all()
 
     talisman.content_security_policy = csp
